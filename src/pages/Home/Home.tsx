6b4edc509691abfc01a6c4775daf4b1d8165c2ca
HaqueArif <haquearif143@gmail.com>
2024-01-29 18:32:29 +0600

    added Valuable client section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./HeroSection";
+import OurClient from "./OurClient";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <OurClient />
     </div>
   );
 };
---
+++ src/pages/Home/OurClient.tsx
@@ -0,0 +1,41 @@
+import soptify from "@/assets/Company/Spotify.svg";
+import google from "@/assets/Company/google.svg";
+import uber from "@/assets/Company/uber.svg";
+import microsoft from "@/assets/Company/microsoft.svg";
+import shopify from "@/assets/Company/shopify.svg";
+import evernote from "@/assets/Company/evernote.svg";
+import adobe from "@/assets/Company/adobe.svg";
+import paypal from "@/assets/Company/paypal.svg";
+import amazon from "@/assets/Company/amazon.svg";
+import asana from "@/assets/Company/asana.svg";
+import Container from "@/components/Container";
+
+const OurClient = () => {
+  const logos = [
+    soptify,
+    google,
+    uber,
+    microsoft,
+    shopify,
+    evernote,
+    adobe,
+    paypal,
+    amazon,
+    asana,
+  ];
+  return (
+    <Container>
+      <div className="mt-[68px] mb-14">
+        <h1 className="text-center mb-[84px]">Our Valuable Client</h1>
+
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-x-2 place-items-center">
+          {logos.map((logo, i) => (
+            <img key={i} src={logo} className="mb-10 sm:mb-24 " alt="" />
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default OurClient;
